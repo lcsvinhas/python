a9f3cdf3f563b0536410a98d8310366673354db9
Lucas Vinhas <l.vinhas.lv@gmail.com>
2025-01-03 09:43:55 -0300

    adicionei cores no ex035

--- exercicios/curso-em-video/ex035.py
+++ exercicios/curso-em-video/ex035.py
@@ -1,10 +1,10 @@
 print('-=-' * 20)
-print('Analisador de Triângulos')
+print('\033[1;34;46mAnalisador de Triângulos\033[m')
 print('-=-' * 20)
 pseg = float(input('Primeiro segmento: '))
 sseg = float(input('Segundo segmento: '))
 tseg = float(input('Terceiro segmento: '))
 if pseg < sseg + tseg and sseg < pseg + tseg and tseg < pseg + sseg:
-    print('Os segmentos acima PODEM FORMAR um triângulo!')
+    print('Os segmentos acima \033[32mPODEM FORMAR\033[m um triângulo!')
 else:
-    print('Os segmentos acima NÃO PODEM FORMAR um triângulo!')
+    print('Os segmentos acima \033[31mNÃO PODEM FORMAR\033[m um triângulo!')
